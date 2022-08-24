46d2f061daf3402627d663aa66dc1bc5765cd0ca
Rowida Essam <rowidaessam21@gmail.com>
2022-08-24 20:29:20 +0200

    AddAdmin

--- identityWithChristina/identityWithChristina/Controllers/AccountController.cs
+++ identityWithChristina/identityWithChristina/Controllers/AccountController.cs
@@ -98,7 +98,7 @@ namespace identityWithChristina.Controllers
         }
 
         //addAdmin
-        [Authorize (Roles ="Admin")]
+   
         public IActionResult AddAdmin()
         {
             return View();
